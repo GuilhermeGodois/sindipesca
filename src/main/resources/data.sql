d4534010b7e44882b0163eaae83b96919c5ad81a
GUILHERME DE MATTOS GODOI <guilherme.godoi@univillebr.onmicrosoft.com>
2020-10-22 19:58:10 +0000

    Implementacaojtwparte1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,6 @@
+delete from usuario;
+INSERT IGNORE INTO usuario(id,usuario,senha) values(1, 'admin','admin');
 INSERT INTO USUARIO(nome,sexo,data_nascimento) values('Guilherme', 'Masculino', '2000-03-17');
 INSERT INTO USUARIO(nome,sexo,data_nascimento) values('Pedro', 'Masculino', '2003-08-24');
 INSERT INTO USUARIO(nome,sexo,data_nascimento) values('Maria', 'Feminino', '2001-05-05');
-INSERT IGNORE INTO usuario(id,usuario,senha) values(1, 'admin','admin');
+
